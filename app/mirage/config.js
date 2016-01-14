8a48bf7dfee5672cceed46ac4438c09ac3f93b44
Stefan <stefan@neuland.io>
2016-01-14 17:30:52 +0100

    synopsis, basic tranformation

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -3,13 +3,40 @@ export default function() {
     return {
       visits: [{
         id: "abcd",
+        active: true,
         host: "localhost",
         visited_at: 1,
         duration: 2000
       }, {
         id: 5,
+        host: "www.spiegel.de",
+        visited_at: 1356776234236,
+        active: true,
+        duration: 2000
+      }, {
+        id: 6,
+        host: "localhost",
+        visited_at: 2342376243236,
+        duration: 2000
+      }, {
+        id: 7,
+        host: "www.heise.de",
+        visited_at: 1234237243236,
+        duration: 3000
+      }, {
+        id: 8,
+        host: "www.spiegel.de",
+        visited_at: 1342376243236,
+        duration: 1000
+      }, {
+        id: 9,
         host: "",
-        visited_at: 12342376243236,
+        visited_at: 1242376243236,
+        duration: 2000
+      }, {
+        id: 10,
+        host: "",
+        visited_at: 1234236243236,
         duration: 2000
       }]
     };
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('visit');
+    return this.store.findAll('synopsis');
   },
 });
