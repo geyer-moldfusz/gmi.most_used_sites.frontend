edd78ecae78fe93aa4f80eb6f3e8a172e69665e6
Stefan <stefan@neuland.io>
2016-01-18 19:06:28 +0100

    build frontend by grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,21 +6,48 @@ module.exports = function(grunt) {
     copy: {
       fonts: {
         files: [{expand: true, cwd: 'app/css/', src: ['fonts/**/*.woff'], dest: 'build/css/'}]
+      },
+      svgs: {
+        files: [{expand: true, cwd: 'app/css/', src: ['svg/*.svg'], dest: 'build/css/'}]
+      },
+      partials: {
+        files: [{expand: true, cwd: 'app/partials/', src: ['*.html'], dest: 'build/partials/'}]
+      },
+      index: {
+        src: 'app/production.html',
+        dest: 'build/index.html'
       }
     },
-    uglify: {
+    concat: {
+      trckyrslf: {
+        src: ['app/js/**.js'],
+        dest: 'build/js/trckyrslf.js'
+      }
+    },
+    cssmin: {
       trckyrslf: {
         files: {
-          'build/js/trckyrslf.min.js': ['app/js/**.js']
+          'build/css/trckyrslf.min.css': ['app/css/app.css']
         }
       }
+    },
+    bower: {
+      options: {
+        targetDir: 'build/vendor/',
+        bowerOptions: {
+          production: true
+        }
+      },
+      install: { }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-bower-task');
 
-  grunt.registerTask('build', ['copy', 'uglify']);
+  grunt.registerTask('build', ['copy', 'concat', 'cssmin', 'bower']);
 
 };
