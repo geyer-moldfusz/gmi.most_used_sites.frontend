ff2bdb15e1784a934cf6e98fd2abd14d565405ca
Stefan <stefan@neuland.io>
2016-02-02 05:28:41 +0100

    fix timing issue on bootstrap

--- app/js/app.js
+++ app/js/app.js
@@ -26,5 +26,6 @@ trckyrslfApp.config(['$routeProvider',
 ]);
 
 window.addEventListener('message', function(event) {
+  while (!angular.element($("#content")).scope()) {}
   angular.element($("#content")).scope().uuid(event.data);
 });
